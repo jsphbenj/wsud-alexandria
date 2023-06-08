7edc6b460dbc3517eeaa910f7928bb915c5f818f
jsphbenj <132235582+jsphbenj@users.noreply.github.com>
2023-06-08 10:19:52 +0200

    Update basin_clip.py

--- basin_clip.py
+++ basin_clip.py
@@ -6,25 +6,41 @@ arcpy.env.overwriteOutput = True
 
 # input variables
 arcpy.env.workspace = r"C:\Users\joeyb\OneDrive\Public\Documents\ArcGIS\Projects\DAAD1\DAAD1.gdb"
+land_class_gdb = r"C:\Users\joeyb\OneDrive\Public\Documents\ArcGIS\Projects\DAAD1\MBLandClass.gdb"
 bounds_fc = "basins1"
 landclass_fc = "LandClip"
 
-with arcpy.da.SearchCursor(bounds_fc, ("Name", "OBJECTID")) as cursor:
+with arcpy.da.SearchCursor(bounds_fc, ("Name")) as cursor:
     for row in cursor:
         # Makes a landclass layer for every MB
-        mb_landclass_clip = row[0].replace("MB ", "landclass_")
-        where = "'Name' = " + "'" + row[0] + "'"
-        mb_selection_fc = "mb_selection"
-        arcpy.management.MakeFeatureLayer(bounds_fc, mb_selection_fc, where)
-        arcpy.analysis.Clip(landclass_fc, mb_selection_fc, mb_landclass_clip)
+        mb_landclass_clip = land_class_gdb + "\\" + row[0].replace("MB ", "landclass_")
+        where = "Name = '{}'".format(row[0])
 
-        # Calculate Area for EACH Land Class Type
-        print(mb_landclass_clip + " Land Classification:")
-        with arcpy.da.UpdateCursor(mb_landclass_clip, ("Class_name", "Area")) as cursorU:
-            for rowU in cursorU:
-                print("FOR LOOP 2 INITIATED")
-                arcpy.management.CalculateGeometryAttributes(mb_landclass_clip, ["Area", "AREA_GEODESIC"],
-                                                             area_unit="SQUARE_KILOMETERS")
-                cursor.updateRow(class_type)
-                print(class_type[0] + ": " + class_type[1])
-                print("")
+        # Create a feature layer from the bounds_fc
+        arcpy.management.MakeFeatureLayer(bounds_fc, "bounds_layer")
+
+        # Apply the selection to the feature layer
+        arcpy.management.SelectLayerByAttribute("bounds_layer", "NEW_SELECTION", where)
+
+        # Create a new feature layer with the selected features
+        arcpy.management.CopyFeatures("bounds_layer", "mb_selection")
+
+        # Create feature layer objects for clipping
+        landclass_layer = arcpy.management.MakeFeatureLayer(landclass_fc, "landclass_layer")
+
+        # Perform the clip operation
+        arcpy.analysis.Clip(landclass_layer, "mb_selection", mb_landclass_clip)
+        print("CLIPPED: " + mb_landclass_clip)
+
+# Calculate Area for EACH Land Class Type
+
+arcpy.env.workspace = r"C:\Users\joeyb\OneDrive\Public\Documents\ArcGIS\Projects\DAAD1\MBLandClass.gdb"
+mb_fc = arcpy.ListFeatureClasses()
+for fc in mb_fc:
+    arcpy.CalculateGeometryAttributes_management(fc, [["Area", "AREA_GEODESIC"]], area_unit="SQUARE_KILOMETERS")
+    print(fc)
+    with arcpy.da.UpdateCursor(fc, ("Class_name", "Area")) as cursorU:
+        for rowU in cursorU:
+            rounded_area = round(rowU[1], 2)
+            print(rowU[0] + ": " + str(rounded_area))
+        print("")
