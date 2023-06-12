f0f0b446bdd38d58f4807f46f43576755fb2f5f4
jsphbenj <132235582+jsphbenj@users.noreply.github.com>
2023-06-12 11:54:34 +0200

    Update dead_end_counter.py

--- dead_end_counter.py
+++ dead_end_counter.py
@@ -1,23 +1,25 @@
-# This script will export all of the dead-end roads within a polygon,
-# then count the number of features to give the number of "culdesacs"/
-# dead end roads within each micro-basin (mb)
+# Created by: Joseph Benjamin
+# Date: June 2023
+
+# Objective: This script will export all of the dead-end roads within a polygon,
+# then count the number of features to give the number of "culdesacs",
+# aka dead end roads, within each micro-basin (mb)
 
 import arcpy
 arcpy.env.overwriteOutput = True
 
-# input variables hard
+# Input Variables
 arcpy.env.workspace = r"C:\Users\joeyb\OneDrive\Public\Documents\ArcGIS\Projects\DAAD1\DAAD1.gdb"
 bounds_fc = "basins1"
 roads_fc  = "roads_and_bound"
 
-# identify the dead ends in fc
+# Identify the dead ends in the roads, creating a new 'dangle' feature class.
 dangle_fc = "dangle_pts"
 arcpy.management.FeatureVerticesToPoints(roads_fc, dangle_fc, "DANGLE")
 total_de_count = arcpy.management.GetCount(dangle_fc)
 print("Total, " + str(total_de_count))
-dead_end_check = []
 
-# dead ends in each microbasin
+# For each micro-basin, find and print the number of dead ends.
 with arcpy.da.SearchCursor(bounds_fc, ("Name")) as cursor:
     for row in cursor:
         basin_fc = row[0].replace(" ", "_")
@@ -25,11 +27,4 @@ with arcpy.da.SearchCursor(bounds_fc, ("Name")) as cursor:
         arcpy.conversion.ExportFeatures(bounds_fc, basin_fc, where)
         dangle_select = arcpy.management.SelectLayerByLocation(dangle_fc, "WITHIN", basin_fc)
         basin_de_count = arcpy.management.GetCount(dangle_select)
-        dead_end_check.append(basin_de_count)
         print(str(basin_fc) + ", " + str(basin_de_count))
-
-for ele in range(0, len(dead_end_check)):
-    total_check = 0
-    total_check += int(dead_end_check[ele])
-
-print("Total Check," + str(total_check))
