fe185a96c526d7ab97ab85e21111c93054ad0aaf
jsphbenj <132235582+jsphbenj@users.noreply.github.com>
2023-07-13 16:01:18 +0200

    Polar Histogram

---
+++ street_orientation/street_orientation.py
@@ -0,0 +1,144 @@
+# Created 11 July 2023 by Joseph Benjamin
+
+import os, pyproj, csv
+import geopandas as gpd
+
+def line_bearing(gdf):
+    index = 0
+    for geometry in gdf.geometry:
+        # Treats MLSs different from typical LineStrings
+        if geometry.geom_type == 'MultiLineString':
+            gdf.at[index, 'fwd_bear'] = ''
+            gdf.at[index, 'back_bear'] = ''
+        else:
+            x, y = geometry.coords.xy
+            long1, lat1 = x[0], y[0]
+            long2, lat2 = x[-1], y[-1]
+            geodesic = pyproj.Geod(ellps='WGS84')
+            fwd_bearing, back_bearing, distance = geodesic.inv(long1, lat1, long2, lat2)
+
+            # Make it so they are both positive
+            if fwd_bearing < -5:
+                fwd_bearing += 360
+            elif fwd_bearing > 355:
+                fwd_bearing -= 360
+            if back_bearing < -5:
+                back_bearing += 360
+            elif back_bearing > 355:
+                back_bearing -= 360
+
+            # Create new columns in the GeoDataFrame
+            gdf.at[index, 'fwd_bear'] = fwd_bearing
+            gdf.at[index, 'back_bear'] = back_bearing
+
+        index += 1
+
+# Set the working directory to the street_orientation folder
+os.chdir(r'C:\Users\joeyb\OneDrive\Public\Documents\GitHub\wsud-alexandria\street_orientation')
+
+# Read the shapefiles
+basin_bounds = gpd.read_file(r'downloads\basins\basins.shp')
+all_roads = gpd.read_file(r'downloads\Roads_all\Roads_all.shp')
+
+# 4. CLIP STREETS TO EACH MICROBASIN
+for index, row in basin_bounds.iterrows():
+    # perform the clip function
+    clipped = gpd.clip(all_roads, row.geometry)
+    print('Clip success!')
+    clipped = clipped[clipped.geometry.type != 'Point']
+
+    # put that clip into a new file
+    mb_number = row['Name'].replace("MB ", "")
+    file_name = 'C:\\Users\\joeyb\\OneDrive\\Public\\Documents\\GitHub\\wsud-alexandria\\street_orientation\\outputs\\' \
+                'roads_clipped_by_mb\\clipped_roads_' + mb_number + '.shp'
+
+    clipped.to_file(file_name)
+    print('Created: ' + file_name)
+print('Streets Clipped!')
+print()
+
+# 5. Calculate Line Bearings
+for root, directories, files in os.walk(r'C:\Users\joeyb\OneDrive\Public\Documents\GitHub\wsud-alexandria\street_orientation\outputs\roads_clipped_by_mb'):
+        for file in files:
+            if file.endswith('.shp'):
+                # Removing irrelevant columns
+                gdf = gpd.read_file('C:\\Users\\joeyb\\OneDrive\\Public\\Documents\\GitHub\\wsud-alexandria\\street_orientation\\outputs\\roads_clipped_by_mb\\' + file)
+                print('Processing ' + file + ':')
+                columns_list = gdf.columns.tolist()
+                removefromlist = ["full_id", "osm_id", "osm_type", "alt_name_e", "name_en", "oneway", "geometry",
+                                  "highway"]
+                updated_list = [x for x in columns_list if x not in removefromlist]
+                gdf = gdf.drop(columns=updated_list)
+                print('Irrelevant columns dropped.')
+
+                # Line bearings calculated with function above
+                line_bearing(gdf)
+                print('Line bearing calculated.')
+
+                # After the line bearings have been calculated, save the gdf to file
+                mb_number = file.replace('clipped_roads_', '')
+                new_file_path = r'C:\Users\joeyb\OneDrive\Public\Documents\GitHub\wsud-alexandria\street_orientation' \
+                                r'\outputs\shp_w_line_bearings\line_bearing_' + mb_number
+                gdf.to_file(new_file_path)
+                print('New file created with line bearing.')
+                print('')
+
+# 6. Calculate Bins in MB Layers
+# 6.1. Read csv and create dictionary based off of it with the bin name, degree range, and the bearings that fall into
+#      that classification. The bearings_list was kept empty so this dictionary can be reused.
+bins = {}
+bin_key_path = r'C:\Users\joeyb\OneDrive\Public\Documents\GitHub\wsud-alexandria\street_orientation\outputs\bin_key.csv'
+with open(bin_key_path, 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)  # skip header row
+    for row in reader:
+        bin_name = row[0]
+        mini = int(row[1])
+        maxi = int(row[2])
+        bearings_list = []
+        bins[bin_name] = (mini, maxi, bearings_list)
+print('Bins created.')
+
+library = 'C:\\Users\\joeyb\\OneDrive\\Public\\Documents\\GitHub\\wsud-alexandria\\street_orientation\\outputs' \
+          '\\shp_w_line_bearings\\'
+for root, directories, files in os.walk(library):
+    for file in files:
+        if file.endswith('.shp'):
+            gdf = gpd.read_file(library + file)
+            # extract the basin number
+            start_str = "line_bearing_"
+            end_str = ".shp"
+
+            start_index = file.index(start_str) + len(start_str)
+            end_index = file.index(end_str)
+
+            mb_number = file[start_index:end_index]
+
+            for index, row in gdf.iterrows():
+                for bin in bins:
+                    # place the fwd_bearing into the appropriate bearing_list in the dictionary
+                    mini = bins[bin][0]
+                    maxi = bins[bin][1]
+                    bearing = float(gdf.at[index, 'fwd_bear'])
+                    try:
+                        if mini <= int(round(bearing)) < maxi:
+                            bins[bin][2].append(bearing)
+                    except:
+                        x = 0
+            # Create new columns in basins boundary shp for each bin.
+            mb_name = 'MB ' + mb_number
+            mb_index = basin_bounds.index[basin_bounds['Name'] == mb_name][0]
+            for bin in bins:
+                basin_bounds.at[mb_index, bin] = int(len(bins[bin][2]))
+            print('Bearings placed in bins for ' + file)
+# Save to new csv, shp
+basin_bins_name = r'C:\Users\joeyb\OneDrive\Public\Documents\GitHub\wsud-alexandria\street_orientation' \
+                  r'\outputs\basin_bins\basin_bins'
+basin_bounds.to_file(basin_bins_name + '.shp')
+basin_bounds.to_csv(basin_bins_name + '.csv')
+
+
+
+
+# todo Use Bins to Create Polar Histogram
+# use matplotlib
